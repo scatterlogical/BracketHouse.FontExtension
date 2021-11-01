c2e6248349407eb65428c04ee4e5f971a4f5f9e0
Peewi <laursen.lau@gmail.com>
2021-11-02 00:12:42 +0100

    Processing of atlas data

--- FontExtension/FontDescription.cs
+++ FontExtension/FontDescription.cs
@@ -8,10 +8,10 @@ namespace FontExtension
         public FontDescription(string path, params char[] characters)
         {
             this.Path = path;            
-            this.Characters = characters.ToList().AsReadOnly();
+            this.Characters = characters;
         }
 
         public string Path { get; }        
-        public IReadOnlyList<char> Characters { get; }
+        public char[] Characters { get; }
     }
 }
